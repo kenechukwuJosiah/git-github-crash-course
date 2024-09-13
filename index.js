c3204d983390e9a95ad0adae9c14d5dc43716348
kenechukwuJosiah <onwe.josiah@appmartgroup.com>
2024-09-13 15:21:24 +0100

    staged all files

---
+++ index.js
@@ -0,0 +1,74 @@
+// //Operators
+
+// /**
+//   ===
+//   ==
+//   =
+//   ||
+//   &&
+//   !
+//   ?
+//   :
+//   <
+//   >
+//   >=
+//   =<
+//   %
+//   *
+//   +
+//   -
+//   **
+//   ++
+//   --
+//  */
+// const citizenip = "Congo";
+// const age = 40;
+// let num = 4;
+
+// const result = citizenip !== "Nigerian" && age >= 18;
+
+// console.log(8 && 8);
+
+// // console.log("Ressult: ", result);
+// // console.log("Ressult: ", result);
+// // console.log("Inc After", ++num);
+// // console.log("Ressult: ", num);
+// // console.log("Inc before", ++num);
+// // console.log("Ressult: ", num);
+// // console.log("Ressult: ", num--);
+// // console.log("Ressult: ", num);
+// const question = "what is your name? \n \n";
+// const ans = "My name is Optimius!!!";
+
+// console.log("Result 1 ${question}", question + ans);
+// console.log(`${question} \n ${ans} \n ${result}`);
+
+// const water = false;
+// const drinkWater = water ? "Drink water" : "Fetch water and drink";
+
+// let result1;
+// function myfunction(str) {
+//   result1 = `Declare a variable *${str}* that uses the + operator to store the result of adding the values stored in a and b`;
+//   return result1;
+// }
+
+// // console.log("Function: ", myfunction(2, 175));
+// // console.log("Function: ", myfunction(2, 2));
+// // console.log("Function: ", myfunction(2, 8));
+// // console.log("Function: ", myfunction(2, 0));
+// // console.log("Function: ", myfunction(2, 12));
+
+// const funcVar = myfunction("add");
+
+// console.log("Function result: ", funcVar, result1);
+
+// const myfunction2 = () => "Hello World!";
+
+// const myfunction3 = () => {
+//   return "Hello World! 22222";
+// };
+
+// console.log(myfunction2());
+// console.log(myfunction3());
+
+// // console.log("Result: ", drinkWater);
